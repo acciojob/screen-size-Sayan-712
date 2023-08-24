e190dcd8cfe461e8a52d15e94fedbb033c77507e
Sayan-712 <134630207+Sayan-712@users.noreply.github.com>
2023-08-25 03:49:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function UpdateWindowSize()
+	{
+	const widthspan=document.getElementById('width');
+	const heightspan=document.getElementById('height');
+		widthspan.textContent=window.innerWidth;
+		heightspan.textContent=window.innerHeight;
+}
+UpdateWindowSize();
+window.addEventListener('resize',UpdateWindowSize);
